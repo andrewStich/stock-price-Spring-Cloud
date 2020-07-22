3c7ee737333e90e60d709f2c0668559f09b8af6b
andrewStich <andrewstich@csus.edu>
2020-07-22 12:30:56 -0700

    added Eureka and Stock Services

--- db-service/src/main/java/com/nagazlabs/stock/dbservice/DbServiceApplication.java
+++ db-service/src/main/java/com/nagazlabs/stock/dbservice/DbServiceApplication.java
@@ -2,8 +2,10 @@ package com.nagazlabs.stock.dbservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableEurekaClient
 @EnableJpaRepositories(basePackages = "com.nagazlabs.stock.dbservice.repository")
 @SpringBootApplication
 public class DbServiceApplication {
---
+++ stock-service/src/main/java/com/nagazlabs/stock/stockservice/recource/StockRecource.java
@@ -0,0 +1,79 @@
+package com.nagazlabs.stock.stockservice.recource;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import yahoofinance.Stock;
+import yahoofinance.YahooFinance;
+
+@RestController
+@RequestMapping("/rest/stock")
+public class StockRecource {
+	
+	@Autowired
+	RestTemplate restTemplate;
+
+	@GetMapping("/{username}")
+	public List<Quote> getStock(@PathVariable("username") final String userName) {
+		
+		ResponseEntity<List<String>> quoteResponse = restTemplate.exchange("http://db-service/rest/db/" + userName, HttpMethod.GET, null, new ParameterizedTypeReference<List<String>>() {});
+	
+		List<String> quotes = quoteResponse.getBody();
+		return quotes
+                .stream()
+                .map(quote -> {
+                    Stock stock = getStockPrice(quote);
+                    return new Quote(quote, stock.getQuote().getPrice());
+                })
+                .collect(Collectors.toList());
+	
+	}
+	
+	private Stock getStockPrice(String quote) {
+		try {
+			return YahooFinance.get(quote);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new Stock(quote);
+	}
+	
+	private class Quote{
+		private String quote;
+		private BigDecimal price;
+		
+		public Quote(String quote, BigDecimal price) {
+			this.quote = quote;
+			this.price = price;
+		}
+
+		public String getQuote() {
+			return quote;
+		}
+
+		public void setQuote(String quote) {
+			this.quote = quote;
+		}
+
+		public BigDecimal getPrice() {
+			return price;
+		}
+
+		public void setPrice(BigDecimal price) {
+			this.price = price;
+		}
+		
+		
+	}
+}
